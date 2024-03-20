2f7ce9ed522f360343705535c311fff49e5e37ec
Georgios Kitsikoudis <georgios.kitsikoudis@csoc.de>
2024-03-20 10:16:30 +0100

    feat(commands): add commands for generating and running go tests

--- lua/gkits/core/commands.lua
+++ lua/gkits/core/commands.lua
@@ -22,3 +22,40 @@ vim.api.nvim_create_user_command("TmuxSession", function()
         vim.cmd("silent !tmux neww tmux-session")
     end)
 end, {})
+
+vim.api.nvim_create_user_command("GoGenTest", function()
+    pcall(function()
+        vim.cmd("!gotests -only <cword> -w %")
+    end)
+end, {})
+
+vim.api.nvim_create_user_command("GoGenAllTests", function()
+    pcall(function()
+        vim.cmd("!gotests -all -w %")
+    end)
+end, {})
+
+vim.api.nvim_create_user_command("GoTest", function(opts)
+    local cmd = "tmux neww -n GoTest 'go test -race -cover " .. opts.args .. " 2>&1 | less'"
+    pcall(function()
+        vim.cmd("!" .. cmd)
+    end)
+end, { nargs = 1, complete = "dir" })
+
+vim.api.nvim_create_user_command("GoTestCover", function(opts)
+    local path = vim.split(opts.args, "/")
+    local outpath = vim.fn.getcwd() .. "/out/"
+
+    if not vim.fn.isdirectory("outpath") then
+        print("directory" .. outpath .. " does not exist")
+        return
+    end
+    outpath = outpath .. path[#path - 1] .. ".out"
+
+    local cmd = "tmux neww -n GoTest 'go test -race -coverprofile " .. outpath .. " " .. opts.args .. " 2>&1 | less'"
+
+    pcall(function()
+        vim.cmd("silent !" .. cmd)
+        vim.cmd("silent !go tool cover -html=" .. outpath)
+    end)
+end, { nargs = 1, complete = "dir" })
