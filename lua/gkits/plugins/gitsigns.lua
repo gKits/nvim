0909d7042aae31d7058a47fcc4a72e3308f40c1b
Georgios Kitsikoudis <georgios.kitsikoudis@csoc.host>
2024-06-20 18:54:41 +0200

    refactor(gitsigns): change gitsigns

--- lua/gkits/plugins/gitsigns.lua
+++ lua/gkits/plugins/gitsigns.lua
@@ -4,11 +4,13 @@ return {
     config = true,
     opts = {
         signs = {
-            add = { text = "+" },
-            change = { text = "~" },
+            add = { text = "┃" },
+            change = { text = "┃" },
             delete = { text = "_" },
             topdelete = { text = "‾" },
             changedelete = { text = "~" },
+            untracked = { text = "┆" },
         },
+        current_line_blame = true,
     },
 }
