b0f025e342ac841987009a6cd07e655fdc7bdbe9
gKits <56302678+gKits@users.noreply.github.com>
2024-07-11 15:32:43 +0200

    refactor(fugitive): remove toggle git status keybind for now

--- lua/gkits/plugins/fugitive.lua
+++ lua/gkits/plugins/fugitive.lua
@@ -2,8 +2,18 @@ return {
     "tpope/vim-fugitive",
     "tpope/vim-rhubarb",
 
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open [g]it ui [s]?" }),
+    vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = { "open git status ui" } }),
     vim.keymap.set("n", "<leader>mt", "<cmd>Gvdiffsplit!<cr>", { desc = "Open [m]erge[t]ool" }),
     vim.keymap.set("n", "<leader>mh", "<cmd>diffget //2<cr>", { desc = "Select to merge from left" }),
     vim.keymap.set("n", "<leader>ml", "<cmd>diffget //3<cr>", { desc = "Select to merge from right" }),
+
+    -- vim.keymap.set("n", "<leader>gs", function()
+    --     for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    --         if vim.api.nvim_buf_get_name(bufnr):find("^fugitive://") then
+    --             vim.api.nvim_buf_delete(bufnr, { force = false })
+    --             return
+    --         end
+    --     end
+    --     vim.cmd.Git()
+    -- end, { desc = { "toggle git status ui" } }),
 }
