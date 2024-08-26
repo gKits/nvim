e41d67a6f59120d6463cdc001ae7caabe736ec25
gKits <56302678+gKits@users.noreply.github.com>
2024-08-26 08:33:11 +0200

    fix(linting): allow parallel runners for golangci-lint

--- lua/gkits/plugins/linting.lua
+++ lua/gkits/plugins/linting.lua
@@ -10,6 +10,9 @@ return {
             json = { "jsonlint" },
         }
 
+        local gocilint = lint.linters.golangcilint
+        gocilint.args = { "--allow-parallel-runners" }
+
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
