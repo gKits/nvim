ce400c12833ccad73577a19336384b8180e8314f
gKits <56302678+gKits@users.noreply.github.com>
2024-12-13 08:17:24 +0100

    feat(oas-preview): add config

--- lua/gkits/plugins/oas-preview.lua
+++ lua/gkits/plugins/oas-preview.lua
@@ -2,6 +2,10 @@ return {
     "rusagaib/oas-preview.nvim",
     event = { "BufEnter *.yml", "BufEnter *.yaml" },
     config = function()
-        require("oas-preview")
+        require("oas-preview").setup({
+            api_route = "http://127.0.0.1",
+            port = "1337",
+            ui = "swagger",
+        })
     end,
 }
