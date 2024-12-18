1a00cf1540cd0e7973aa533f995683bf255db701
gKits <56302678+gKits@users.noreply.github.com>
2024-12-18 10:47:54 +0100

    feat(filetypes): add sub filetypes for yaml and json

--- lua/gkits/core/filetypes.lua
+++ lua/gkits/core/filetypes.lua
@@ -1,5 +1,12 @@
 vim.filetype.add({
     extension = {
         templ = "templ",
+        env = "dotenv",
     },
+    pattern = {
+        ["*compose.ya?ml"] = "yaml.docker-compose",
+        ["openapi.*%.ya?ml"] = "yaml.openapi",
+        ["openapi.*%.json"] = "json.openapi",
+    },
+    filename = {},
 })
