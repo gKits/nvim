10df4bae0e0791b3b8e664c7a8ffe9d60dadc724
gKits <56302678+gKits@users.noreply.github.com>
2024-12-24 09:26:41 +0100

    feat(filetypes): add specific yaml filetypes

--- lua/gkits/core/filetypes.lua
+++ lua/gkits/core/filetypes.lua
@@ -1,12 +1,30 @@
+local findfile = vim.fn.findfile
+local cwd = vim.fn.getcwd
+
 vim.filetype.add({
+    pattern = {
+        ["openapi.*%.ya?ml"] = "yaml.openapi",
+        ["openapi.*%.json"] = "json.openapi",
+        ["*%compose.ya?ml"] = "yaml.docker-compose",
+    },
+    filename = {
+        [".gitlab-ci.yml"] = "yaml.gitlab",
+        [".gitlab-ci.yaml"] = "yaml.gitlab",
+    },
     extension = {
         templ = "templ",
         env = "dotenv",
+        yaml = function(_, _, _)
+            if findfile("ansible.cfg", cwd()) ~= "" then
+                return "yaml.ansible"
+            end
+            return "yaml"
+        end,
+        yml = function(_, _, _)
+            if findfile("ansible.cfg", cwd()) ~= "" then
+                return "yaml.ansible"
+            end
+            return "yaml"
+        end,
     },
-    pattern = {
-        ["*compose.ya?ml"] = "yaml.docker-compose",
-        ["openapi.*%.ya?ml"] = "yaml.openapi",
-        ["openapi.*%.json"] = "json.openapi",
-    },
-    filename = {},
 })
